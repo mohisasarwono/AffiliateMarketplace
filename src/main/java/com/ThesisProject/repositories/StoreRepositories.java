b258610b8b8b18fb2dc9f8fffc954b2d136f612b
mohisasarwono <mohisa.sarwono@binus.ac.id>
2021-07-06 14:53:43 +0700

    Fixing advance Search 3

--- src/main/java/com/ThesisProject/repositories/StoreRepositories.java
+++ src/main/java/com/ThesisProject/repositories/StoreRepositories.java
@@ -26,24 +26,24 @@ public interface StoreRepositories extends JpaRepository<Store, Long> {
     List<Object[]> getAllDataForMarketplace(@Param("storeIds") List<Long> storeIds);
     
     @Query(value="select i.id as iId from mst_store as s "
-            + "join mst_item as i on s.id = i.store_id where s.name ilike(concat('%',:name,'%')) and i.status = 1 and s.status = 1 order by i.id ", nativeQuery = true)
+            + "join mst_item as i on s.id = i.store_id where s.name ilike(concat('%',:name,'%')) and i.status = 1 and s.status = 1 and i.expired_date >= now() order by i.id ", nativeQuery = true)
     List<Long> getItemByNStoreName(@Param("name")String name);
     
     @Query(value="select i.id as iId from mst_store as s "
-            + "join mst_item as i on s.id = i.store_id where i.name ilike(concat('%',:name,'%')) and i.status = 1 order by i.id ", nativeQuery = true)
+            + "join mst_item as i on s.id = i.store_id where i.name ilike(concat('%',:name,'%')) and i.status = 1 and i.expired_date >= now() order by i.id ", nativeQuery = true)
     List<Long> getItemByItemName(@Param("name")String name);
     
     @Query(value="select i.id as iId from mst_item as i "
-            + "where i.type ilike(:type) and i.status = 1 order by i.id", nativeQuery = true )
+            + "where i.type ilike(:type) and i.status = 1 and i.expired_date >= now() order by i.id", nativeQuery = true )
     List<Long> getItemByType(@Param("type")String type);
     
     @Query(value="select i.id from mst_store as s " +
     "join mst_item as i on s.id = i.store_id " +
     "join mst_peripheral as p on p.item_id = i.id " +
     "join trx_commission as c on c.peripheral_id = p.id " +
-    "where c.total_transaction >= :totalTranscation and s.status = 1 order by i.id ", nativeQuery = true)
+    "where c.total_transaction >= :totalTranscation and s.status = 1 and i.expired_date >= now() order by i.id ", nativeQuery = true)
     List<Long> getItemByTotalTransaction(@Param("totalTranscation")Integer totalTransaction);
     
-     @Query(value="select id from mst_item where commission_price_or_percentage>=:commissionRange and commission_status =:commissionStatus and status= 1 order by id ", nativeQuery = true)
+    @Query(value="select id from mst_item where commission_price_or_percentage>=:commissionRange and commission_status =:commissionStatus and status= 1 and expired_date >= now() order by id ", nativeQuery = true)
     List<Long> getItemByCommissionRange (@Param("commissionRange")Double commissionRange, @Param("commissionStatus") Byte comStat);
 }
